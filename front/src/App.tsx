7320353464aa1aed819db670991d787f1f938ae1
KimJacobus <jacobus.kim@gmail.com>
2023-02-13 14:16:03 +0100

    preset

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,22 @@
 
-
-export function App() {
+function App() {
   return (
+
+
+
+<body className="bg-slate-grey">
+
+
     <h1 className="text-3xl font-bold underline">
+
+
       Hello world!
     </h1>
+
+
+    </body>
+
+
   )
 }
 export default App
