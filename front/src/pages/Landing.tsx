ebe0a68e725b4a4fd8de9f2a36f44a2123a4a100
KimJacobus <jacobus.kim@gmail.com>
2023-02-15 22:33:50 +0100

    landingpage/Emailuser

--- front/src/pages/Landing.tsx
+++ front/src/pages/Landing.tsx
@@ -24,15 +24,19 @@ const Landing = () => {
         <div className='flex justify-center'>
             
             <div id="FFTitle" className='absolute z-10'>
-                <h1 className='righteous text-3xl lg:text-8xl text-white mt-10 text-center'>Welcome to <br></br>Film Friendzy!</h1>
-                <p className='jose text-2xl lg:text-5xl text-white mt-4 text-center'>Movie nights with friends from anywhere!</p>
+                <h1 className='righteous text-3xl lg:text-8xl text-white mt-10 mb-10 text-center'>Welcome to <br></br>Film Friendzy!</h1>
 
-        <div className="mt-10 opacity-90">
+                <p className='jose text-2xl lg:text-3xl text-white text-center mb-28'>Movie nights with friends from anywhere!</p>
 
-            <h1 className='text-1xl text-white text-center'>already have an account ?</h1>
+
+
+
+
+                <p className='jose text-sm lg:text-2xl text-white mb-1 text-center'>Sign up now and get a friend to join for free!</p>
+
+
 
                 <Emailuser />
-                </div>
 
 
 
