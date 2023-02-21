3098f91baf35af7760eee10d77a07dee50945150
KimJacobus <jacobus.kim@gmail.com>
2023-02-21 20:26:51 +0100

    smartphone fetch works

--- front/src/composants/Carousel/SmallCarousel.tsx
+++ front/src/composants/Carousel/SmallCarousel.tsx
@@ -1,68 +1,40 @@
+
 import { useSnapCarousel } from 'react-snap-carousel';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import React,{ useState, useEffect } from 'react';
 import LazyImage from '../Carousel/LazyLoad/LazyImage';
 
 
-
 const SmallCarousel = () => {
-
-
   const { scrollRef } = useSnapCarousel();
   const [activeItem, setActiveItem] = useState(null);
   const [items, setItems] = useState<any[]>([]);
-  const [check, setCheck] = useState();
-
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/trending/all/day?api_key=83a1629902bd9dbacb7cf2bcff2293ab&language=fr-FR')
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      setItems(data);
+    const fetchImages = async () => {
+      const response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=83a1629902bd9dbacb7cf2bcff2293ab&query=a');
+      const data = await response.json();
+      const results = data.results.map((item: any) => item);
+      setItems(results);
 
-// do filter options here ?
-      
-      const results = data.results
-      console.log(results)
-
-      setCheck(results.poster_path);
-    })
+    };
+    fetchImages();
   }, []);
-
-
-
-
-  console.log(items)
-  // if(items) {
-    
-    // console.log(items.results[0].poster_path);
-
-const film = "https://image.tmdb.org/t/p/w400" + check ;
-
-
-  films = [{}]
-
+// put depency here ? for refreshing on search 
 
 
 
 
   const handleClick = (index: any) => {
     if (activeItem === index) {
-      setActiveItem(null)
-      
+      setActiveItem(null);
     } else {
-
-    setActiveItem(index)
+      setActiveItem(index);
     }
   };
 
-
-
   return (
     <ul
-
       ref={scrollRef}
       style={{
         display: 'flex',
@@ -70,43 +42,29 @@ const film = "https://image.tmdb.org/t/p/w400" + check ;
         scrollSnapType: 'x mandatory',
         scrollbarWidth: 'none',
         cursor: 'grab',
-        userSelect: 'none'
+        userSelect: 'none',
       }}
     >
-
-
-      {Array.from({ length: 10 }).map((_, i) => (
-          <div
-
-          
+      {items.map((item, i) => (
+        <div
           key={i}
-          
-          // style={{backgroundImage: `url('${items.length > 0 ?  film : ''})` }}  
+          style={{
 
-          style={{backgroundImage: `url(${film})`}}  
+            backgroundImage: `url('https://image.tmdb.org/t/p/w400${item.poster_path}')`,
 
-
-
+          }}
           className={`righteous flex bg-cyan-400 text-3xl w-[250px] h-[350px] rounded-md m-5 shrink-0 text-white 
           justify-center items-center cursor-grab  select-none transition-all duration-500
-         
-          
           ${activeItem === i ? 'active: w-[350px] active: h-[600px] transition-transform' : ''}`}
-          onClick={() => handleClick(i)}>
-          Item {i}
-
-
+          onClick={() => handleClick(i)}
+        >
+          <h1 className="text-xl text-center">{item.title}</h1> 
         </div>
       ))}
-
-
     </ul>
   );
-
-
 };
-
-export default SmallCarousel;
+export default SmallCarousel
 
 
 
